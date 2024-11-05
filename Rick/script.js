6bcb6729652a212135e690e6b236049aab9a5ab0
Wesleyhenrique543 <wh16122002@gmail.com>
2024-11-05 11:28:12 -0300

    Add files via upload

---
+++ Rick/script.js
@@ -0,0 +1,31 @@
+const fetchCharacters = async () => {
+    try {
+        const response = await fetch('https://rickandmortyapi.com/api/character/?page=19');
+        const data = await response.json();
+        displayCharacters(data.results);
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+    }
+};
+
+const displayCharacters = (characters) => {
+    const cardContainer = document.getElementById('card-container');
+    characters.forEach(character => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        card.innerHTML = `
+            <img src="${character.image}" alt="${character.name}">
+            <h2>${character.name}</h2>
+            <p>Status: ${character.status}</p>
+            <p>Espécie: ${character.species}${character.type ? ` (${character.type})` : ''}</p>
+            <p>Origem: ${character.origin.name}</p>
+            <p>Localização: ${character.location.name}</p>
+        `;
+
+        cardContainer.appendChild(card);
+    });
+};
+
+
+fetchCharacters();
